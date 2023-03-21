82671192a2c5f91633ec656269c9d900951f31ec
akashdasp <akash.dasp@gmail.com>
2023-03-21 18:23:14 +0530

    logger and exception

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 def error_massage_details(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
@@ -7,7 +8,13 @@ def error_massage_details(error,error_details:sys):
     return error_message
 class CustomException(Exception):
     def __init__(self,error_message,error_details: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_massage_details(error_message,error_details=error_details)
     def __str__(self):
         return self.error_message
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)
