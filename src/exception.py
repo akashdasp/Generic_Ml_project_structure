32dc49c4511b5bbde78cdd81c55f09edcdc87295
akashdasp <akash.dasp@gmail.com>
2023-03-23 11:00:21 +0530

    'addingnotebook,data_injetion'

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_massage_details(error,error_details:sys):
+def error_massage_details(error,error_details: sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message='Error occured om python script name [{0}], line number[{1}] error mssage details:[{2}]'.format(file_name,exc_tb.tb_lineno,str(error))
--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from datetime import datetime
-
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 log_path=os.path.join(os.getcwd(),'logs',LOG_FILE)
 os.makedirs(log_path,exist_ok=True)
@@ -9,6 +8,6 @@ os.makedirs(log_path,exist_ok=True)
 LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s %(levelname)s - %(massage)s",
+    format="[%(asctime)s ] %(lineno)d %(name)s %(levelname)s - %(massage)s",
     level=logging.INFO,
 )
